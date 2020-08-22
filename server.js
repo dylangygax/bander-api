3e56000b9a648703ffcbbdec19500792361b54ec
DylanGygax <dylangygax@ncf.edu>
2020-08-22 13:39:15 -0500

    got app listening on port

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 //require
+const express = require('express')
+const app = express()
+const PORT = /*process.env.PORT ||*/ 4000
 
 //middleware
 
+
 //connection
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
