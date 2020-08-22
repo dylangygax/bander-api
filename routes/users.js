121367bf413d53457f6338d17c9adc979ece5b25
DylanGygax <dylangygax@ncf.edu>
2020-08-22 15:55:57 -0500

    controllers set up started. index route successfully sends placeholder text to browser

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ const router = require('express').Router()
 const ctrl = require('../controllers')
 
 // routes
-// index show create update destroy
+// index filter show create update destroy
 router.get('/', ctrl.users.index)
-router.get('/filter/:id', ctrl.users.filter)
-router.get('/:id', ctrl.users.show)
-router.post('/', ctrl.users.create)
-router.put('/:id', ctrl.users.update)
-router.delete('/:id', ctrl.users.destroy)
+// router.get('/filter/:id', ctrl.users.filter)
+// router.get('/:id', ctrl.users.show)
+// router.post('/', ctrl.users.create)
+// router.put('/:id', ctrl.users.update)
+// router.delete('/:id', ctrl.users.destroy)
 
 // exports
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //require
 const express = require('express')
 const app = express()
+const routes = require('./routes')
+//const cors = require('cors')
 const PORT = /*process.env.PORT ||*/ 4000
 
 //middleware
+app.use(express.json()) //JSON parsing
+//app.use(cors())
 
+//routes
+app.use('/api/v1/users', routes.users)
 
 //connection
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
