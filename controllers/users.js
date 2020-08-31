e82bc6afee3f3ed33e011090341e6920f2a2eb51
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-30 17:15:36 -0700

    updated user Schema

--- controllers/users.js
+++ controllers/users.js
@@ -7,41 +7,42 @@ const index = (req, res) => {
      db.User.find({}, (err, foundUsers) => {
           if (err) console.log(`error in users#index: ${err}`)
           //res.send('users index called')
-          res.status(200).json({users: foundUsers})
+          res.status(200).json({ users: foundUsers })
      })
 }
 
 const results = (req, res) => {
      db.User.find(
-          { 
+          {
                $or: [
-                    {genres: { $in: req.body.genres }},
-                    {instruments: { $in: req.body.instruments }}
+                    { genres: { $in: req.body.genres } },
+                    { instruments: { $in: req.body.instruments } }
                ]
-          } , (err, foundUsers) => {
-          if (err) console.log(`error in users#filter: ${err}`)
-          console.log("request received", req.body);
-          res.status(200).json({users: foundUsers})
-     });
+          }, (err, foundUsers) => {
+               if (err) console.log(`error in users#filter: ${err}`)
+               console.log("request received", req.body);
+               res.status(200).json({ users: foundUsers })
+          });
 }
 
-const show = (req,res) => {
+const show = (req, res) => {
      db.User.findById(req.params.id, (err, foundUser) => {
           if (err) console.log(`error in users#show: ${err}`)
           //res.send(`user show called`)
-          res.status(200).json({user: foundUser})
+          res.status(200).json({ user: foundUser })
      })
 }
 
 const create = (req, res) => {
-     db.User.create(req.body, (err,createdUser) => {
+     db.User.create(req.body, (err, createdUser) => {
           if (err) console.log(`error in users#create: ${err}`)
-          res.status(200).json({user: createdUser})
+          res.status(200).json({ user: createdUser })
      })
 }
 
 const update = (req, res) => {
-     db.User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
+     db.User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedUser) => {
+          console.log("userId: " + req.params.id);
           if (err) console.log(`error in users#update: ${err}`)
           res.status(200).json({
                user: updatedUser,
